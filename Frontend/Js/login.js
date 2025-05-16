9325fe33851684cfb7bf471fcd11eaadb43d2a18
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-05-16 16:08:55 -0300

    css

--- Frontend/Js/login.js
+++ Frontend/Js/login.js
@@ -1,17 +1,22 @@
+// Seletores
 var body = document.querySelector("body");
+var container = document.querySelector(".container");
 var signInButton = document.querySelector("#signIn");
 var signUpButton = document.querySelector("#signUp");
 
-body.onload = function(){
+// Efeito ao carregar a página
+body.onload = function () {
     body.className = "on-load";
-}
+};
 
-signUpBtn.addEventListener('click', () => {
+// Evento para "Cadastrar" (mostrar formulário de cadastro)
+signUpButton.addEventListener('click', () => {
     container.classList.add('sign-up');
-    container.classList.remove('signIn');
+    container.classList.remove('sign-in');
 });
 
-signInBtn.addEventListener('click', () => {
+// Evento para "Entrar" (mostrar formulário de login)
+signInButton.addEventListener('click', () => {
     container.classList.add('sign-in');
     container.classList.remove('sign-up');
 });
