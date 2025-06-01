aacbdb37798a7010c19f2686cb147cbfc61add02
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-06-01 02:54:52 -0300

    remove backend

--- Backend/umfgcloud.autenticacao.dominio/Interfaces/Servicos/IAutenticacaoServico.cs
+++
@@ -1,9 +0,0 @@
-﻿using umfgcloud.autenticacao.dominio.DTO;
-
-namespace umfgcloud.autenticacao.dominio.Interfaces.Servicos;
-
-public interface IAutenticacaoServico
-{
-    Task RegistrarUsuarioAsync(UsuarioDTO.RegistrarRequest dto);
-    Task<UsuarioDTO.AutenticarResponse> AutenticarUsuarioAsync(UsuarioDTO.AutenticarRequest dto);
-}
